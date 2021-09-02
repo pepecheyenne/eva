d59359ce948ff7c21e99a79d4a36c7bf125946a6
pepecheyenne <58045783+pepecheyenne@users.noreply.github.com>
2021-09-02 11:46:46 -0500

    Create monitoring.fs

---
+++ monitoring.fs
@@ -0,0 +1,17 @@
+type    : agent.filesystem
+label   : Filesystem - /
+period  : 60
+timeout : 10
+details :
+    target : /
+alarms  :
+    disk-size:
+        label: Usage on /
+        notification_plan_id: npXXXXXXXX
+        criteria: |
+            if (percentage(metric['used'], metric['total']) > 90) {
+                return new AlarmStatus(CRITICAL, 'Disk usage is above 90%, #{used} out of #{total}');
+            }
+            if (percentage(metric['used'], metric['total']) > 80) {
+                return new AlarmStatus(WARNING, 'Disk usage is above 80%, #{used} out of #{total}');
+            }
