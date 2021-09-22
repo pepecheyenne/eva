5e0a9eac46a944a124d6b8c24e95d5bb6d6fac9e
Pepe Cheyenne <pepe.orozco@gmail.com>
2021-09-22 15:58:40 -0500

    .

--- minerinstall.sh
+++ minerinstall.sh
@@ -102,7 +102,9 @@ read -p "Press enter to continue ..."
 echo "#######################################"
 echo -e "Configuring keyfile password in service file.\n"
 echo "#######################################"
-read -s "Enter your keyfile password and press enter: " varpass
+stty -echo
+read "Enter your keyfile password and press enter: " varpass
+stty echo
 sudo echo -e $varpass > "${vardirnode}/pp${varnode}"
 varPassFile="${vardirnode}/pp${varnode}"
 echo -e "keyfile.json password updated ...\n\n"
